7d06d273b67d0781dbad8c69ed0c276a3bbde89b
BLaurenB <blbillington1@gmail.com>
2017-10-17 16:11:55 -0600

    merge issues

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
-RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Comment do
+  describe "validations" do
+
+    context "invalid attributes" do
+      it "is invalid without content" do
+        comment = Comment.create()
+        expect(comment).to be_invalid
+      end
+    end
+
+    context "valid attributes" do
+      it "is valid with content" do
+        comment = create(:comment)
+        expect(comment).to be_valid
+      end
+    end
+
+  end
+
+  describe "relationships" do
+
+    it " belongs to one job" do
+      comment = create(:comment)
+      expect(comment).to respond_to(:jobs)
+    end
+  end
 end
